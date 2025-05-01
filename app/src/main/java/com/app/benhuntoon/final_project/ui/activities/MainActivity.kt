6d2d66c62a6e243711f9dc73a8237e1f061181cf
benhu <benhuntoon@outlook.com>
2025-04-30 20:22:32 -0700

    Functional Nav bar with test fragments

--- app/src/main/java/com/app/benhuntoon/final_project/ui/activities/MainActivity.kt
+++ app/src/main/java/com/app/benhuntoon/final_project/ui/activities/MainActivity.kt
@@ -2,18 +2,47 @@ package com.app.benhuntoon.final_project.ui.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.app.benhuntoon.final_project.R
+import com.app.benhuntoon.final_project.databinding.ActivityMainBinding
+import com.app.benhuntoon.final_project.ui.fragments.Home
+import com.app.benhuntoon.final_project.ui.fragments.saved
 import com.app.benhuntoon.final_project.ui.fragments.CreateMadLibFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(Home())
 
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, CreateMadLibFragment())
-                .commit()
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.home -> replaceFragment(Home())
+                R.id.saved -> replaceFragment(saved())
+
+                //where to send users for other pages
+                else ->{
+
+                }
+            }
+            true
         }
+
+//        if (savedInstanceState == null) {
+//            supportFragmentManager.beginTransaction()
+//                .replace(R.id.fragment_container, CreateMadLibFragment())
+//                .commit()
+//        }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
     }
 }
